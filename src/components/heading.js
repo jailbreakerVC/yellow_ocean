fddafcf91debad246e87f9819cb8895532dbf31d
jailbreakerVC <54243183+jailbreakerVC@users.noreply.github.com>
2022-04-05 06:44:30 +0530

    update the README.md with link to contributing guide

---
+++ src/components/heading.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Component } from "react";
+
+const Heading = () => <h1 style={{color: 'orange'}} align='center'>Welcome to Yellow Ocean Official! </h1>
+
+export default Heading
---
+++ src/components/image.js
@@ -0,0 +1,5 @@
+import React, {Component} from "react";
+
+const Image = () => <img src={'ocean.jpeg'} height='1000' width='800' align="center"></img>
+
+export default Image
