20b5bc55984be31f28178485313520c00552827d
VIJIT CHANDNA <vijit123chandna@gmail.com>
2022-04-20 11:50:09 +0530

    lol

--- src/components/image.js
+++ src/components/image.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
 
-const Image = () => <img src={'ocean.jpeg'} height='1000' width='800' align="center"></img>
+const Image = () => <img src={'ocean.jpeg'} width='100%' height='100%' marginTop='-70px' backgroundSize= 'cover' backgroundRepeat= 'no-repeat'></img>
 
 export default Image
